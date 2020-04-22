7b713de2dd27ff31de5806c3dd9f9d2638d95deb
Reginaldo Pereira dos Santos <reginaldopereira@reginaldos-mbp.home>
2020-04-22 17:16:46 +0100

    6-mongodb

--- mongo-node-express-1/server.js
+++ mongo-node-express-1/server.js
@@ -13,7 +13,21 @@ app.listen(3000, function() {
     res.sendFile(__dirname + '/index.html')
  })
  app.post('/quotes', (req, res) => { 
-    console.log(req.body)
-  })
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+        console.log('saved to database')
+        res.redirect('/')
+      })
+    })
+
+  
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://test:<password>@cluster0-ky6dr.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 
